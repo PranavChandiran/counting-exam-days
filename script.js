ce345e2e482997c18b3fc0dc0bc41284c177da16
Pranav Chandiran <pranavchandiranj16@gmail.com>
2022-11-26 14:04:16 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+// today=new Date();
+// var cmas=new Date(2023, 1,1);
+// if (today.getMonth()==11 && today.getDate()>25) 
+// {
+// cmas.setFullYear(cmas.getFullYear()+1); 
+// }  
+// var one_day=1000*60*60*24;
+// console.log(Math.ceil((cmas.getTime()-today.getTime())/(one_day))+
+// " days left until Christmas!");
+
+
+
+window.onload = function() {
+    let  showit = document.querySelector("#daysleft");
+    let Today = new Date();
+    let boardMonth =new Date(2023, 1,1);
+    
+    if (Today.getMonth()==11 && today.getDate()>25) 
+    {
+    cmas.setFullYear(boardMonth.getFullYear()+1); 
+    }  
+    
+    let fullday = 1000*60*60*24;
+    
+    let final = (Math.ceil((boardMonth.getTime()-Today.getTime())/(fullday)));
+    document.getElementById("daysleft").innerHTML= final;
+}
