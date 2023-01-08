bbac0de51006624c9937861ae6fee4ed1f603bb3
Pranav Chandiran <pranavchandiranj16@gmail.com>
2023-01-08 22:03:57 +0530

    update v1

--- script.js
+++ script.js
@@ -11,17 +11,24 @@
 
 
 window.onload = function() {
-    let  showit = document.querySelector("#daysleft");
+    let  showing = document.querySelector("#daysleft");
     let Today = new Date();
-    let boardMonth =new Date(2023, 1,1);
+    let boardMonth = new Date(2023, 1, 23);
     
     if (Today.getMonth()==11 && Today.getDate()>25) 
     {
-    cmas.setFullYear(boardMonth.getFullYear()+1); 
+    Today.setFullYear(boardMonth.getFullYear()+1); 
     }  
     
     let fullday = 1000*60*60*24;
     
     let final = (Math.ceil((boardMonth.getTime()-Today.getTime())/(fullday)));
     document.getElementById("daysleft").innerHTML= final;
+
+ if ( final < 25) {
+
+    document.body.style.backgroundColor = "#F40B00";
+
+ }
+    
 }
