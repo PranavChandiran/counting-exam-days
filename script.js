ce0862be9a6bfb2bb31d8e3d95971c127d913e13
pranav16 <93902331+PranavChandiran@users.noreply.github.com>
2023-01-08 21:10:50 +0530

    fixed again

--- script.js
+++ script.js
@@ -13,7 +13,7 @@
 window.onload = function() {
     let  showit = document.querySelector("#daysleft");
     let Today = new Date();
-    let boardMonth =new Date(23,2,2023);
+    let boardMonth =new Date("2023-02-23");
     
     if (Today.getMonth()==11 && Today.getDate()>25) 
     {
