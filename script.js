f3edc534e5e91f545a14d0c5c185d6802f846b84
pranav16 <93902331+PranavChandiran@users.noreply.github.com>
2023-01-22 11:42:46 +0530

    v2

--- script.js
+++ script.js
@@ -1,23 +1,39 @@
 
 
-window.onload = function() {
-    let  showing = document.querySelector("#daysleft");
+// window.onload = function() {
+//     let  showing = document.querySelector("#daysleft");
+//     let Today = new Date();
+//     let boardMonth = new Date(2023, 1, 23);
+    
+//     if (Today.getMonth()==11 && Today.getDate()>25) 
+//     {
+//     Today.setFullYear(boardMonth.getFullYear()+1); 
+//     }  
+    
+//     let fullday = 1000*60*60*24;
+    
+//     let final = (Math.ceil((boardMonth.getTime()-Today.getTime())/(fullday)));
+//     document.getElementById("daysleft").innerHTML   = final;
+
+//     if ( final < 25) {
+
+//         document.body.style.backgroundColor = "#F40B00";
+    
+//      }
+// }
+
+
+setInterval(function(){
     let Today = new Date();
     let boardMonth = new Date(2023, 1, 23);
-    
-    if (Today.getMonth()==11 && Today.getDate()>25) 
-    {
-    Today.setFullYear(boardMonth.getFullYear()+1); 
-    }  
-    
     let fullday = 1000*60*60*24;
-    
     let final = (Math.ceil((boardMonth.getTime()-Today.getTime())/(fullday)));
     document.getElementById("daysleft").innerHTML   = final;
-
     if ( final < 25) {
-
         document.body.style.backgroundColor = "#F40B00";
-    
-     }
-}
+    }
+    if (Today.getTime() > boardMonth.getTime()) {
+        document.body.style.backgroundColor = "#000000";
+        document.getElementById("message").innerHTML = "The event has passed";
+    }
+}, 86400000);
