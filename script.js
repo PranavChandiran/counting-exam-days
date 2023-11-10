737ec3433466ec9be1531a19da0dcb0b36be2ced
pranav16 <93902331+PranavChandiran@users.noreply.github.com>
2023-11-11 02:39:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
 document.addEventListener('DOMContentLoaded', function () {
-  // Target date: 12.11.23
-  const targetDate = new Date('2023-11-12').getTime();
+  // Target dates
+  const hostelLifeDate = new Date('2023-06-14').getTime();
+  const tenthDate = new Date('2023-02-24').getTime();
 
-  // Update the countdown every second
-  setInterval(function () {
-    const currentDate = new Date().getTime();
-    const timeDifference = currentDate - targetDate;
+  // Function to update countdown
+  function updateCountdown(targetDate, elementId) {
+    setInterval(function () {
+      const currentDate = new Date().getTime();
+      const timeDifference = targetDate - currentDate;
 
-    const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+      const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
 
-    document.getElementById('countdown').innerHTML = `Days since 12.11.23: ${days}`;
-  }, 1000);
+      document.getElementById(elementId).innerHTML = `Days remaining: ${days}`;
+    }, 1000);
+  }
+
+  // Update countdown for hostel life
+  updateCountdown(hostelLifeDate, 'hostelCountdown');
+
+  // Update countdown for 10th
+  updateCountdown(tenthDate, 'tenthCountdown');
 });
